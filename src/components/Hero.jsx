26abcd871b5de0e02e8f854747ec565e22467eea
Dhairya Rathod <143081566+Dhairya-coder-spec@users.noreply.github.com>
2026-08-26 18:14:05 +0530

    implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,16 @@
 import gsap from 'gsap';
 import {useGSAP} from "@gsap/react";
 import { SplitText } from 'gsap/all';
+import {useRef} from "react";
+import {useMediaQuery} from "react-responsive";
 
 const Hero = () => {
 
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({maxWidth: 767})
+
     useGSAP(() => {
-        const heroSplit = new SplitText('.tittle', {type: 'chars, words'})
+        const heroSplit = new SplitText('.title', {type: 'chars, words'})
         const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
 
         heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
@@ -35,13 +40,33 @@ const Hero = () => {
             }
         })
             .to('.right-leaf', { y: 200 }, 0)
-            .to('.left-leaf', { y: -200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0);
+
+        const startValue = isMobile? 'top 50%' : 'center 60%';
+        const endValue = isMobile? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
+
     }, []);
 
     return (
         <>
             <section id="hero" className="noisy">
-                <h1 className="tittle">MOJITO</h1>
+                <h1 className="title">MOJITO</h1>
 
                 <img
                     src="/images/hero-left-leaf.png"
@@ -65,13 +90,23 @@ const Hero = () => {
 
                         <div className="view-cocktails">
                             <p className="subtitle">
-                                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+                                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes -  designed to delight your senses.
                             </p>
                             <a href="#cocktails">View Cocktails</a>
                         </div>
                     </div>
                 </div>
             </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+            </div>
         </>
     );
 };
